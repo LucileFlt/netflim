6091e61cd607052ce57d711ea7919d05729beccc
Estherlvn <estherlvn@gmail.com>
2024-07-22 15:17:30 +0200

    Update NavBarHeader.jsx

--- src/components/NavBarHeader/NavBarHeader.jsx
+++ src/components/NavBarHeader/NavBarHeader.jsx
@@ -8,7 +8,6 @@ const NavBarHeader = () => {
     <nav className={styles.navbarheader}>
       <ul className={styles.navList}>
         <li className={styles.navItem}><Link to="/" className={styles.navLink}>Accueil</Link></li>
-        <li className={styles.navItem}><Link to="/movie-detail" className={styles.navLink}>Détail</Link></li> {/* Nouveau lien vers la page de détail */}
         <li className={styles.navItem}><Link to="/watchlist" className={styles.navLink}>À voir</Link></li>
         <li className={styles.navItem}><Link to="/favorites" className={styles.navLink}>Favoris</Link></li>
       </ul>
