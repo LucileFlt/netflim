b9e7a1b0a8ffe65bb2de36cc799d21b0eb084a67
lucilefonlupt <fonlupt.lucile@gmail.com>
2024-07-23 15:27:46 +0200

    Watchlist.jsx

--- src/pages/Watchlist-Page/Watchlist.jsx
+++ src/pages/Watchlist-Page/Watchlist.jsx
@@ -1,4 +1,51 @@
-const Watchlist =() =>{
-    return(<h1>Watchlist</h1>)
+import React, { useState, useEffect } from 'react';
+import { getMovieDetails } from '../../API/note-api';
+import styles from './style.module.css';
+
+const Watchlist = () => {
+  const [watchlistMovies, setWatchlistMovies] = useState([]);
+
+  useEffect(() => {
+    loadWatchlistMovies();
+  }, []);
+
+  const loadWatchlistMovies = async () => {
+    const watchlistIds = JSON.parse(localStorage.getItem('watchlist')) || [];
+    const watchlistMoviesData = await Promise.all(watchlistIds.map(id => getMovieDetails(id)));
+    setWatchlistMovies(watchlistMoviesData);
+  };
+
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + '...';
     }
-    export default Watchlist;
+    return text;
+  };
+
+  return (
+    <div className={styles.pageContainer}>
+      <h2 className={styles.pageTitle}>Watchlist</h2>
+      <div className={styles.moviesGrid}>
+        {watchlistMovies.map(movie => (
+          <div key={movie.id} className={styles.movieCard}>
+            <img 
+              src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} 
+              alt={movie.title} 
+              className={styles.movieImage} 
+            />
+            <div className={styles.movieDetails}>
+              <h3 className={styles.movieTitle}>{movie.title}</h3>
+              <p className={styles.movieOverview}>{truncateText(movie.overview, 80)}</p>
+              <p className={styles.movieRating}>Note: {movie.vote_average}</p>
+              <a href={`/movie-detail-page/${movie.id}`} className={styles.detailsLink}>Voir les détails</a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Watchlist;
+
+
